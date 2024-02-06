cad3148f4c9c520b643f45bcdda47a4c638dce2e
SRS <rs.sku@yandex.ru>
2024-02-06 13:18:38 +0300

    task3 fixed

--- task3.py
+++ task3.py
@@ -1,26 +1,28 @@
-with open('1.txt', encoding='utf-8') as f:
-    data1 = f.readlines()
-    full_data1 = [f.name + '\n', f'{len(data1)}\n']
-    full_data1 += data1
-    full_data1[-1] += '\n'
-
-with open('2.txt', encoding='utf-8') as f:
-    data2 = f.readlines()
-    full_data2 = [f.name + '\n', f'{len(data2)}\n']
-    full_data2 += data2
-    full_data2[-1] += '\n'
-
-with open('3.txt', encoding='utf-8') as f:
-    data3 = f.readlines()
-    full_data3 = [f.name + '\n', f'{len(data3)}\n']
-    full_data3 += data3
-
-with open('task3_result.txt', 'w', encoding='utf-8') as f:
-    f.writelines(full_data1)
-    f.writelines(full_data2)
-    f.writelines(full_data3)
+import os
 
 
+def get_txt_filenames(directory):
+    filenames = os.listdir(directory)
+    for filename in filenames:
+        if not filename.endswith('.txt'):
+            filenames.remove(filename)
+    return filenames
 
 
+def make_array(filenames, directory):
+    full_data = []
+    for file_name in filenames:
+        with open(directory + "/" + file_name, encoding='utf-8') as f:
+            file_data = f.readlines()
+            full_data.append([file_name, len(file_data), file_data])
+    full_data.sort(key=lambda x: x[1])
+    for data in full_data[:-1]:
+        data[2][-1] += '\n'
 
+    return full_data
+
+
+with open('task3_result.txt', 'w', encoding='utf-8') as f_result:
+    for data_ in make_array(get_txt_filenames('task3 files'), 'task3 files'):
+        f_result.write(f'{data_[0]}\n{data_[1]}\n')
+        f_result.writelines(data_[2])
